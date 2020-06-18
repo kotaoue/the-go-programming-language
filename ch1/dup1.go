1a30c538c2939b0e2cd3453f279b873294f3f037
kota oue <kota.oue@yappli.co.jp>
2020-06-18 22:24:29 +0900

    Update dup1.go

--- ch1/dup1.go
+++ ch1/dup1.go
@@ -10,13 +10,12 @@ func main() {
 	counts := make(map[string]int)
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
-		fmt.Println("ok")
 		counts[input.Text()]++
-	}
 
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+		for line, n := range counts {
+			if n > 1 {
+				fmt.Printf("%d\t%s\n", n, line)
+			}
 		}
 	}
 }

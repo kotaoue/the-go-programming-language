014ad7c8a7a6a5eabe963f75e572fd28f6cb4e04
kota oue <kota.oue@yappli.co.jp>
2020-06-20 22:45:14 +0900

    Update exercise1_5.go

--- ch1/exercise1_5.go
+++ ch1/exercise1_5.go
@@ -15,8 +15,8 @@ import (
 var palette = []color.Color{color.Black, color.RGBA{0x00, 0xff, 0x00, 0xff}}
 
 const (
-	whiteIndex = 0
-	blackIndex = 1
+	blackIndex = 0
+	greenIndex = 1
 )
 
 func main() {
@@ -42,7 +42,7 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)

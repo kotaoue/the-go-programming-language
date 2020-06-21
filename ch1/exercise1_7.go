7f9bc31d3cddf7cc268019eec6f60e9559916497
kota oue <kota.oue@yappli.co.jp>
2020-06-21 14:54:30 +0900

    Update exercise1_7.go

--- ch1/exercise1_7.go
+++ ch1/exercise1_7.go
@@ -11,14 +11,14 @@ import (
 
 func main() {
 	for _, url := range os.Args[1:] {
-		_, err := http.Get(url)
+		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
 
 		r := strings.NewReader("some io.Reader stream to be read\n")
-		_, err = io.Copy(os.Stdout, r)
+		_, err = io.Copy(r, resp.Body)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s; %v\n", url, err)
 			os.Exit(1)

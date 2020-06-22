03e2ef23fdd1940d5596afe4025e74e6bef11ed8
kota oue <kota.oue@yappli.co.jp>
2020-06-22 13:13:26 +0900

    Update fetchall.go

--- ch1/fetchall.go
+++ ch1/fetchall.go
@@ -37,5 +37,5 @@ func fetch(url string, ch chan<- string) {
 	}
 
 	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%.2fs %7d %s", secs.nbytes, url)
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 }

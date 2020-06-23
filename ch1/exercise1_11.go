46946903dc3240072fcdd94c5cb5497c8dc96481
kotaoue <kotaoue@gmail.com>
2020-06-23 23:23:38 +0900

    Copy from exercise1_10.go

---
+++ ch1/exercise1_11.go
@@ -0,0 +1,44 @@
+// e.g. go run exercise1_11.go http://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest;go run exercise1_10.go http://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+
+	file, _ := os.Create(fmt.Sprintf("exercise1_10_%v.txt", time.Now().Unix()))
+	for range os.Args[1:] {
+		fmt.Fprintln(file, <-ch)
+	}
+	fmt.Fprintf(file, "%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}

2fa6987fcb3a1d612dc5fe13826e96e28eaafab5
kotaoue <kotaoue@gmail.com>
2020-06-28 12:59:23 +0900

    Update echo4.go

--- ch2/echo4.go
+++ ch2/echo4.go
@@ -4,15 +4,14 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 	"strings"
 )
 
 var n = flag.Bool("n", false, "omit trailing newline")
-var set = flag.String("s", "", "separator")
+var sep = flag.String("s", "", "separator")
 
 func main() {
-	flag,Parse()
+	flag.Parse()
 	fmt.Print(strings.Join(flag.Args(), *sep))
 	if !*n {
 		fmt.Println()

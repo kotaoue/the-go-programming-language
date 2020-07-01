83b42e4cf79252eb73de4f0640ba300e15eb6833
kotaoue <kotaoue@gmail.com>
2020-07-01 22:57:30 +0900

    Add import temoconv

--- ch2/tempconv_main.go
+++ ch2/tempconv_main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	_ "github.com/kotaoue/the-go-programming-language/ch2/tempconv"
 )
 
 func main() {
-	fmt.Printf("%g\n", 1)
+	fmt.Printf("%g\n", BoilingC-FreezingC)
 }

bbc81a1078b6cfb90972c9a71ff0cc860db80a10
kotaoue <kotaoue@gmail.com>
2020-07-01 23:08:19 +0900

    Update tempconv.go

--- ch2/tempconv/tempconv.go
+++ ch2/tempconv/tempconv.go
@@ -9,5 +9,6 @@ const (
 	BoilingC      Celsius = 100
 )
 
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
-func FToC(f Fahrenheit) Celsius { return Celsius((f - 32)) * 5 / 9 }
+func CToF(c Celsius) Fahrenheit  { return Fahrenheit(c*9/5 + 32) }
+func FToC(f Fahrenheit) Celsius  { return Celsius((f - 32)) * 5 / 9 }
+func (c Celsius) String() string { return fmt.Sptintf("%gC", c) }

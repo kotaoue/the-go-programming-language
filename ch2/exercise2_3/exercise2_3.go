243f5db070de7089020a3fea2491c75a2abc6ea3
kotaoue <kotaoue@gmail.com>
2020-07-06 23:16:53 +0900

    Update exercise2_3.go

--- ch2/exercise2_3/exercise2_3.go
+++ ch2/exercise2_3/exercise2_3.go
@@ -1,3 +1,6 @@
+// e.g $ pwd
+// /go/src/github.com/kotaoue/the-go-programming-language/ch2/exercise2_3
+// go test -bench .
 package main
 
 import (
@@ -8,6 +11,12 @@ import (
 
 func BenchmarkPopCount(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		popcount.PopCount(8)
+		popcount.PopCount(i)
+	}
+}
+
+func BenchmarkPopCountLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCountLoop(i)
 	}
 }

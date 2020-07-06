0c0731714a013baa268e78bc5dbe07de334aaed6
kotaoue <kotaoue@gmail.com>
2020-07-06 22:35:42 +0900

    Update popcount_main.go

--- ch2/popcount_main.go
+++ ch2/popcount_main.go
@@ -8,4 +8,11 @@ import (
 
 func main() {
 	fmt.Printf("%d\n", popcount.PopCount(1))
+	fmt.Printf("%d\n", popcount.PopCount(2))
+	fmt.Printf("%d\n", popcount.PopCount(3))
+	fmt.Printf("%d\n", popcount.PopCount(4))
+	fmt.Printf("%d\n", popcount.PopCount(5))
+	fmt.Printf("%d\n", popcount.PopCount(6))
+	fmt.Printf("%d\n", popcount.PopCount(7))
+	fmt.Printf("%d\n", popcount.PopCount(8))
 }

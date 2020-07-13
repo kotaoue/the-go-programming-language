892675c341eab6d171a9077c24659ab1ff21ff56
kotaoue <kotaoue@gmail.com>
2020-07-13 18:40:51 +0900

    Create xmlselect.go

---
+++ ch7/xmlselect/xmlselect.go
@@ -0,0 +1,47 @@
+// e.g. echo '<?xml version="1.0" encoding="UTF-8" ?><items><item><name>Black Sun</name><color>Black</color></item><item><name>Shadow Moon</name><color>Silver</color></item></items>' | go run xmlselect.go
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	dec := xml.NewDecoder(os.Stdin)
+	var stack []string
+	for {
+		tok, err := dec.Token()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Fprintf(os.Stderr, "xmlselet: %v\n", err)
+			os.Exit(1)
+		}
+		switch tok := tok.(type) {
+		case xml.StartElement:
+			stack = append(stack, tok.Name.Local)
+		case xml.EndElement:
+			stack = stack[:len(stack)-1]
+		case xml.CharData:
+			if containsAll(stack, os.Args[1:]) {
+				fmt.Printf("%s: %s\n", strings.Join(stack, " "), tok)
+			}
+		}
+	}
+}
+
+func containsAll(x, y []string) bool {
+	for len(y) <= len(x) {
+		if len(y) == 0 {
+			return true
+		}
+		if x[0] == y[0] {
+			y = y[1:]
+		}
+		x = x[1:]
+	}
+	return false
+}

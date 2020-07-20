603ea9b98ecfba3b8c5228386661dc42338c2203
kotaoue <kotaoue@gmail.com>
2020-07-20 21:29:45 +0900

    Update popcount_main.go

--- ch2/popcount_main/popcount_main.go
+++ ch2/popcount_main/popcount_main.go
@@ -7,12 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Printf("%d\n", popcount.PopCount(1))
-	fmt.Printf("%d\n", popcount.PopCount(2))
-	fmt.Printf("%d\n", popcount.PopCount(3))
-	fmt.Printf("%d\n", popcount.PopCount(4))
-	fmt.Printf("%d\n", popcount.PopCount(5))
-	fmt.Printf("%d\n", popcount.PopCount(6))
-	fmt.Printf("%d\n", popcount.PopCount(7))
-	fmt.Printf("%d\n", popcount.PopCount(8))
+	for i := 0; i < 64; i++ {
+		fmt.Printf("%d %d %d\n", i, popcount.PopCount(uint64(i)), popcount.PopCountLoop(uint64(i)))
+	}
 }

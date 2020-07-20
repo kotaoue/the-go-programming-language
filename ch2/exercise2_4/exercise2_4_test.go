c9bd4ab8d0f871129a8f927565a425373ddee534
kotaoue <kotaoue@gmail.com>
2020-07-20 21:22:52 +0900

    Copy from exercise2_3_test.

---
+++ ch2/exercise2_4/exercise2_4_test.go
@@ -0,0 +1,20 @@
+// e.g. go test -bench . -benchmem
+package main
+
+import (
+	"testing"
+
+	popcount "github.com/kotaoue/the-go-programming-language/ch2/popcount"
+)
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCount(uint64(i))
+	}
+}
+
+func BenchmarkPopCountLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCountLoop(uint64(i))
+	}
+}
---
+++ ch2/exercise2_4/go.mod
@@ -0,0 +1,7 @@
+module github.com/kotaoue/the-go-programming-language/ch2/exercise2_4
+
+go 1.14
+
+replace github.com/kotaoue/the-go-programming-language/ch2/popcount => ./../popcount
+
+require github.com/kotaoue/the-go-programming-language/ch2/popcount v0.0.0-20200706133542-0c0731714a01

0ac97bba0bd5d047ff0affeabe604215f59cfad8
kotaoue <kotaoue@gmail.com>
2020-08-30 06:33:35 +0900

    Update main.go

--- ch3/ch3-1/main.go
+++ ch3/ch3-1/main.go
@@ -18,4 +18,9 @@ func main() {
 
 	var y uint8 = 1<<1 | 1<<2
 	fmt.Printf("%08b\n", y)
+
+	fmt.Printf("%08b\n", x&y)
+	fmt.Printf("%08b\n", x|y)
+	fmt.Printf("%08b\n", x^y)
+	fmt.Printf("%08b\n", x&^y)
 }

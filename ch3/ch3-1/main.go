d0fac757d0ee86429646ae88984ae1b3eca7f3c5
kotaoue <kotaoue@gmail.com>
2020-08-30 06:37:22 +0900

    Update main.go

--- ch3/ch3-1/main.go
+++ ch3/ch3-1/main.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("--P59--")
-	var u uint8 = 255
-	fmt.Println(u, u+1, u*u)
-
-	var i int8 = 127
-	fmt.Println(1, 1+1, i*i)
+	p59()
 
 	fmt.Println("--P60--")
 	var x uint8 = 1 << 1
@@ -33,4 +28,13 @@ func main() {
 
 	fmt.Printf("%08b\n", x<<1)
 	fmt.Printf("%08b\n", x>>1)
+}
+
+func p59() {
+	fmt.Println("--P59--")
+	var u uint8 = 255
+	fmt.Println(u, u+1, u*u)
+
+	var i int8 = 127
+	fmt.Println(1, 1+1, i*i)
 }

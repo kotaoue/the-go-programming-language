dc6beebb166a75107d1ea29033a4d9ee89f81886
kotaoue <kotaoue@gmail.com>
2020-09-06 16:37:18 +0900

    Update main.go

--- ch3/ch3-2/main.go
+++ ch3/ch3-2/main.go
@@ -7,9 +7,15 @@ import (
 
 func main() {
 	p63_1()
+	p63_2()
 }
 
 func p63_1() {
 	fmt.Printf("%g\n", math.MaxFloat32)
 	fmt.Printf("%g\n", math.MaxFloat64)
+}
+func p63_2() {
+	var f float32 = 16777216
+	fmt.Printf("%g\n", f)
+	fmt.Printf("%t\n", f == f+1)
 }

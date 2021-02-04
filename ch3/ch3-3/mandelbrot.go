4e13576f5e28da38ea6164e02e6fbd4692e831d9
kotaoue <kotaoue@gmail.com>
2021-02-04 19:32:28 +0900

    Create mandelbrot.go

---
+++ ch3/ch3-3/mandelbrot.go
@@ -0,0 +1,51 @@
+// $ go run ch3/ch3-3/mandelbrot.go > ch3/ch3-3/result.png
+// $ open -a '/Applications/Google Chrome.app' ch3/ch3-3/result.png
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin   = -2
+		ymin   = -2
+		xmax   = 2
+		ymax   = 2
+		width  = 1024
+		height = 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height + (ymax - ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+
+	png.Encode(os.Stdout, img)
+}
+
+func mandelbrot(z complex128) color.Color {
+	const (
+		iterations = 200
+		contrast   = 15
+	)
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}

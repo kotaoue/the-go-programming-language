9438c89d36760de2ce085ddbf5c05a17d021e379
kotaoue <kotaoue@gmail.com>
2022-01-25 23:05:12 +0900

    study ch8/spinner

--- ch8/spinner/main.go
+++ ch8/spinner/main.go
@@ -3,23 +3,25 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/tj/go-spin"
 )
 
 func main() {
 	go spinner(100 * time.Microsecond)
-	const n = 40
+	const n = 45
 	fibN := fib(n) // slow
 	fmt.Printf("\rFibonaccci(%d) = %d\n", n, fibN)
 }
 
 func spinner(delay time.Duration) {
+	s := spin.New()
 	for {
-		for _, r := range `-\|/` {
-			fmt.Printf("\r%c", r)
-			time.Sleep(delay)
-		}
+		fmt.Printf("\r  \033[36mcomputing\033[m %s ", s.Next())
+		time.Sleep(delay)
 	}
 }
+
 func fib(x int) int {
 	if x < 2 {
 		return x

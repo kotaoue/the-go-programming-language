47040383534a5af629ab2dd5c6a18876f2860bb8
kotaoue <kotaoue@gmail.com>
2022-01-25 23:06:30 +0900

    study ch8/spinner

--- ch8/spinner/main.go
+++ ch8/spinner/main.go
@@ -9,13 +9,14 @@ import (
 
 func main() {
 	go spinner(100 * time.Microsecond)
-	const n = 45
+	const n = 40
 	fibN := fib(n) // slow
 	fmt.Printf("\rFibonaccci(%d) = %d\n", n, fibN)
 }
 
 func spinner(delay time.Duration) {
 	s := spin.New()
+	s.Set(spin.Spin3)
 	for {
 		fmt.Printf("\r  \033[36mcomputing\033[m %s ", s.Next())
 		time.Sleep(delay)
